8d0cef2d52f402c0a083ac1e515dc47b64b02e95
Drishti Mishra <drishtimishrahere@gmail.com>
2026-02-20 01:41:02 +0530

    fix: correct vercel frontend domain in CORS

--- backend/main.py
+++ backend/main.py
@@ -22,12 +22,12 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://finpulse-ai-8dxwlxqba-drishtipixiees-projects.vercel.app"
-        "https://finpulse-ai-production.up.railway.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 @app.on_event("startup")
 def on_startup():
